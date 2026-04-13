fb6175ba2657d8cc975bded51955b48b433fb341
Momen Noor <momen.noor@gmail.com>
2026-04-13 20:30:05 +0400

    Final Edition phase 7 (Cache Font Size Changer)

--- app/Models/Matter.php
+++ app/Models/Matter.php
@@ -307,6 +307,10 @@ class Matter extends Model
     public function finalReportSubmission(): void
     {
         $this->final_report_at = now();
+        if($this->final_report_memo_date === null)
+        {
+            $this->final_report_memo_date = now();
+        }
         $this->save();
     }
 
