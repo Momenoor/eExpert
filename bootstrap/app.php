1eb1b8a943b4eb27751439970525662e48c71cc2
Momen Noor <momen.noor@gmail.com>
2026-06-09 15:40:20 +0400

    Try to fix exporter
    Signed-off-by: Momen Noor <momen.noor@gmail.com>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $schedule->command('mail:send-bulk-campaigns')->everyMinute();
         $schedule->command('queue:work')->everyFifteenSeconds();
 
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
