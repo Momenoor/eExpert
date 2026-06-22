b7a81ac5562dbb847d07154b668ff733721e70b4
Momen Noor <momen.noor@gmail.com>
2026-06-22 06:01:55 +0400

    Export with cron jobs

--- resources/views/errors/maintenance.blade.php
+++ resources/views/errors/maintenance.blade.php
@@ -4,5 +4,8 @@
 
 @section('title', __('Service Unavailable'))
 @section('code', '503')
-@section('message', 'We are currently undergoing maintenance. Please check back later. \n نحن حالياً نعمل على الصيانة. يرجى التحقق من الموقع لاحقاً.')
-@section('message', '')
+@section('message')
+    We are currently undergoing maintenance. Please check back later.
+    <br>
+    نحن حالياً نعمل على الصيانة. يرجى التحقق من الموقع لاحقاً.
+@endsection
